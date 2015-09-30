f172259e1ab2e149e64ae96262f73252b5db3eac
Francisc Camillo <fjcamillo.dev@gmail.com>
2015-09-30 18:31:12 +0800

    added trialMapper file
    added trialMapper for testing

---
+++ trialMapper.py
@@ -0,0 +1,17 @@
+import c4d
+from c4d import gui
+#Welcome to the world of Python
+
+nullObject = c4d.BaseObject(5140)
+arduino = serial.Serial('COM37', 115200, timeout = .1)
+
+
+
+def main():
+
+    
+
+if __name__=='__main__':
+    main()
+    doc.InsertObject(nullObject)
+    c4de.EventAdd()
