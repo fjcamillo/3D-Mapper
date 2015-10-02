6bac59ee204c2df422a8f46a73fb5fac3eb213f9
Francisc Camillo <fjcamillo.dev@gmail.com>
2015-10-02 20:16:11 +0800

    working trial mapper
    trial mapper works whoooo

--- trialMapper.py
+++ trialMapper.py
@@ -1,21 +1,27 @@
 import c4d
 from c4d import gui
+import math
 #Welcome to the world of Python
 
-nullObject = c4d.BaseObject(5140)
+nullObject = []
+nullCount = 0
 
+def mainProcess():
+	createNullObject()
+	nullObject[nullCount][c4d.ID_BASEOBJECT_REL_ROTATION,c4d.VECTOR_Y] = math.radians(90)
 
+def createNullObject():
+	nullObject.append(c4d.BaseObject(5140))
+	doc.InsertObject( nullObject[nullCount] )
 
-def main():
-	stringFromSerial = arduino.readline()
+def addEvent():
+	c4d.EventAdd()
 
-
-def learnString():
-
-
-    
+def createPointOfOrigin():
+	nullObject.append(c4d.BaseObject(5140))
+	doc.InsertObject( nullObject[nullCount] )
+	nullCount += 1
 
 if __name__=='__main__':
-    main()
-    doc.InsertObject(nullObject)
-    c4de.EventAdd()
+    mainProcess()
+    addEvent()
